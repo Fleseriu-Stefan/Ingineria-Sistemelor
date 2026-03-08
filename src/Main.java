3584b6fd72aee641aeeacf7f4e5d81b1f3f973b7
Fleseriu Stefan <stefanfleseriu@gmail.com>
2026-03-08 14:59:51 +0200

    Lab1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Student a = new Student(578,"Stefan","Fleseriu","ISM");
+        System.out.println(a.toString());
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,15 @@
 public class Student {
+    int numarmatricol;
+    String prenume;
+    String nume;
+    String formatiedestudiu;
+    public Student(int numarmatricol,String prenume,String nume,String formatiedestudiu){
+        this.numarmatricol=numarmatricol;
+        this.prenume=prenume;
+        this.nume=nume;
+        this.formatiedestudiu=formatiedestudiu;
+    }
+    public String toString(){
+        return "Student: "+" "+numarmatricol+" "+prenume+" "+nume+" "+formatiedestudiu;
+    }
 }
